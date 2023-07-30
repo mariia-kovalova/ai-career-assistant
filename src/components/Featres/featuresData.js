bc17413e003207848a6e3fe3d02ee18aba6bb56f
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-07-30 03:35:09 +0300

    services data upg

--- src/components/Featres/featuresData.js
+++ src/components/Featres/featuresData.js
@@ -1,23 +1,44 @@
 export const featuresData = [
   {
-    title: 'Створити CV',
+    title: 'Create a CV',
     content:
-      'Ви зможете побудувати сучасне CV за допомогою Штучного інтелекту. CV буде створюватися динамічно за результатами відповідей на запитання, які Вам поставить асистент у чаті.',
+      'You will have the opportunity to create a modern and professional CV with the assistance of Artificial Intelligence. The AI-powered chat will interact with you, asking specific questions to gather all the necessary information for your CV. The process will be dynamic, meaning that the questions and sections of the CV will adapt based on your responses. This way, you can ensure that your CV is tailored to your skills and experiences. Once the chat-based interview is complete, you will have a polished and well-structured CV ready to showcase your qualifications to potential employers.',
     ariaControls: 'panel1a-content',
     id: 'panel1a-header',
   },
   {
-    title: 'Побувати на імітації співбесіди',
+    title: 'Simulate a Job Interview',
     content:
-      'Ви зможете пройти співбесіду з AI. Тут реалізований концепт живої співбесіди. Асистент може сприймати живу мову та аналізувати Ваші відповіді.',
+      'Through this feature, you can experience a simulated job interview conducted by an AI-based assistant. The AI has the capability to understand natural language and analyze your responses in real-time. This provides a lifelike interview experience, where you can practice answering typical interview questions and receive feedback on your performance. The feedback will include valuable insights into your communication skills, confidence, and other aspects that are crucial for a successful job interview. This immersive experience will help you gain confidence and improve your interview skills, ultimately increasing your chances of landing your desired job.',
     ariaControls: 'panel2a-content',
     id: 'panel2a-header',
   },
   {
-    title: 'Пройти тести з hard skills',
+    title: 'Take Hard Skills Tests',
     content:
-      'Ви зможете пройти тести, які будуть перевірені асистетном. Після цього Ви отримаєте конструктивний фідбек та персоналізовані поради щодо майбутнього розвитку.',
+      'To assess your hard skills, you will be presented with various interactive tests. These tests cover different areas of expertise relevant to your desired career path. The assistant will evaluate your performance in these tests and provide constructive feedback to highlight your strengths and areas for improvement. Additionally, based on your test results, the AI assistant will offer personalized recommendations for your future skill development. This will guide you in choosing the right learning path to enhance your proficiency in specific areas and advance your career in the IT industry.',
     ariaControls: 'panel3a-content',
     id: 'panel3a-header',
+  },
+  {
+    title: 'Track Your Progress and Statistics',
+    content:
+      'The application will keep track of your progress and generate detailed statistics based on your performance in various assessments and tests. You can view your skill development over time, compare your progress with other users in the same field, and identify areas where you excel or need improvement. These insights will help you understand your growth and make data-driven decisions for your career development.',
+    ariaControls: 'panel4a-content',
+    id: 'panel4a-header',
+  },
+  {
+    title: 'Receive Job Recommendations',
+    content:
+      "Based on the information gathered from your CV, test results, and skill assessments, the application's AI will match you with relevant job vacancies that align with your skills and career goals. You will receive personalized job recommendations, making it easier for you to find entry-level positions and opportunities suitable for trainees and juniors in the IT industry.",
+    ariaControls: 'panel5a-content',
+    id: 'panel5a-header',
+  },
+  {
+    title: 'Career Assistance and Guidance',
+    content:
+      "The AI-powered assistant will provide career advice and guidance tailored to your unique aspirations and progress. It will analyze your strengths, weaknesses, and interests to suggest specific steps you can take to develop certain skills and increase your chances of landing your dream job. Whether it's recommending online courses, projects, or certifications, the career assistance feature will be your personalized mentor throughout your job search and professional growth journey.",
+    ariaControls: 'panel6a-content',
+    id: 'panel6a-header',
   },
 ];
